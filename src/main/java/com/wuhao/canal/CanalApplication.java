b0aef2dfc0b80f4afa3fc36002fb7f25d96bcfd9
wuhao <30432963+mikewuhao@users.noreply.github.com>
2021-03-07 23:01:26 +0800

    Update CanalApplication.java

--- src/main/java/com/wuhao/canal/CanalApplication.java
+++ src/main/java/com/wuhao/canal/CanalApplication.java
@@ -3,7 +3,7 @@ package com.wuhao.canal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class CanalApplication {
 
     public static void main(String[] args) {
